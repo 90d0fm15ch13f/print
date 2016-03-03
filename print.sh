7c53b50ee7a7ddb7f7a9c0635d40caf61746d4d7
mtonsmann <m.tonsmann@gmail.com>
2016-03-03 15:42:03 -0700

    removed unnecesary cd commands and added absolute paths

--- print.sh
+++ print.sh
@@ -7,22 +7,18 @@ expect "100%"
 #now ssh into imagine and place the file on a bbbox
 spawn ssh <minesuname>@imagine.mines.edu
 expect "$ "
-send "cd ~/tmp\r"
-expect "$ "
-send "scp toprint <minesuname>@bb136-17:~/tmp\r"
+send "scp ~/tmp/toprint <minesuname>@bb136-17:~/tmp\r"
 expect "100%"
-send "rm toprint\r"
+send "rm ~/tmp/toprint\r"
 expect "$ "
 send "exit\r"
 
 #ssh into imagine and immediately into the bbbox to print
 spawn ssh -t <minesuname>@imagine.mines.edu "ssh <minesuname>@bb136-17"
 expect "$ "
-send "cd ~/tmp\r"
+send "lp -d bb136-printer ~/tmp/toprint\r"
 expect "$ "
-send "lp -d bb136-printer toprint\r"
-expect "$ "
-send "rm toprint\r"
+send "rm ~/tmp/toprint\r"
 expect "$ "
 send "exit\r"
 
