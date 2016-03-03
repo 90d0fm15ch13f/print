df36934b924dd412ae376c3e92b6df358079dbdf
90d0fm15ch13f <godofmischief@earthlink.net>
2016-03-03 15:01:50 -0700

    added setup script so that users don't have to do manual code adjustment

--- print.sh
+++ print.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/expect -f
 
 #should ssh into a bb136 box and then print a file
-spawn scp /home/<localuname>/tmp/toprint <minesuname>@imagine.mines.edu:</path/to/mines/home>/tmp
+spawn scp /home/<localuname>/tmp/toprint <minesuname>@imagine.mines.edu:~/tmp
 expect "100%"
 
+#now ssh into imagine and place the file on a bbbox
 spawn ssh <minesuname>@imagine.mines.edu
 expect "$ "
 send "cd ~/tmp\r"
@@ -14,6 +15,7 @@ send "rm toprint\r"
 expect "$ "
 send "exit\r"
 
+#ssh into imagine and immediately into the bbbox to print
 spawn ssh -t <minesuname>@imagine.mines.edu "ssh <minesuname>@bb136-17"
 expect "$ "
 send "cd ~/tmp\r"
---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sed s/"<localuname>"/"$USER"/ print.sh > print1.sh
+echo -e "Mines Username:"
+read uname
+sed 's/<minesuname>/'"${uname}"'/g' print1.sh > print.sh
+rm print1.sh
+
+echo -e "Local setup complete.\n Be sure that you have shared ssh keys with all the necessary computers.\nIt is also highly recommended that you add an alias"
