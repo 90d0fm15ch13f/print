dc11dc4bfbf4a6a672325f7c9ce343aef9748be8
Benjamintf1 <Benjamintf1@gmail.com>
2016-03-03 20:48:11 -0700

    Yo no need for temporary files.

--- setup.sh
+++ setup.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-sed s/"<localuname>"/"$USER"/ print.sh > print1.sh
 echo -e "Mines Username:"
 read uname
-sed 's/<minesuname>/'"${uname}"'/g' print1.sh > print.sh
-rm print1.sh
+sed -e 's/<minesuname>/'"${uname}"'/g' -e s/"<localuname>"/"$USER"/ print.sh < print1.sh > print.sh
 
 echo -e "Local setup complete.\n  Be sure that you have shared ssh keys with all the necessary computers.\n  It is also highly recommended that you add an alias"
