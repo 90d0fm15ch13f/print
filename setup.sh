b3a0039d8bae0dbe78cc4406fff5e5e13babc51c
90d0fm15ch13f <godofmischief@earthlink.net>
2016-03-06 09:44:37 -0700

    more tinkering with the sed command in setup

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@
 
 echo -e "Mines Username:"
 read uname
-sed -e 's/<minesuname>/'"${uname}"'/g' -e s/"<localuname>"/"$USER"/ print.sh < print1.sh > print.sh
+sed -i -e 's/<minesuname>/'"${uname}"'/g' -e s/"<localuname>"/"$USER"/ print.sh
 
 echo -e "Local setup complete.\n  Be sure that you have shared ssh keys with all the necessary computers.\n  It is also highly recommended that you add an alias"
